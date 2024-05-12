39906f8d98471adeefbf5a0251356009929da287
sam45643 <122574805+sam45643@users.noreply.github.com>
2024-05-12 02:35:12 -0500

    IMAGE_TO_TEXT

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Add necessary using statements
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string subscriptionKey = "800157e6cf96432a8958603d45173bed";
+        string endpoint = "https://new333.cognitiveservices.azure.com/";
+
+        var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(subscriptionKey))
+        {
+            Endpoint = endpoint
+        };
+
+        string localImagePath = @"C:\Users\samde\Downloads\20240327_170557.jpg";
+
+        using (var imageStream = File.OpenRead(localImagePath))
+        {
+            var result = await client.RecognizePrintedTextInStreamAsync(true, imageStream);
+
+            foreach (var region in result.Regions)
+            {
+                foreach (var line in region.Lines)
+                {
+                    foreach (var word in line.Words)
+                    {
+                        Console.Write(word.Text + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
+
